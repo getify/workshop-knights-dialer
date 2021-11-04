9d47f881a1a6d5878a6d4a872c53d18359d445df
Kyle Simpson <getify@gmail.com>
2021-11-04 12:47:30 -0500

    implementing option 1 algorithms

--- js/dialer.js
+++ js/dialer.js
@@ -7,33 +7,89 @@ export default {
 
 // ****************************
 
+var dialpad = [
+	[ 1, 2, 3 ],
+	[ 4, 5, 6 ],
+	[ 7, 8, 9 ],
+	[  , 0,   ]
+];
+
 function reachableKeys(startingDigit) {
-	// TODO: return which digits a Knight's move
-	// can hop to from a given starting digit/key
-	//
-	// e.g. 3 -> [ 4, 8 ]
-	//      4 -> [ 3, 9, 0 ]
-	//      5 -> []
-	return [];
+	var nearbyKeys = [];
+
+	for (let [ rowIdx, row ] of dialpad.entries()) {
+		let colIdx = row.indexOf(startingDigit);
+		if (colIdx != -1) {
+			// check the possible knight moves
+			for (let rowMove of [ -2, -1, 1, 2 ]) {
+				for (let colMove of [ -2, -1, 1, 2 ]) {
+					// only consult the combinations where the
+					// row/col move magnitudes are different
+					//
+					// e.g., no moves of (2,2) or (2,-2),
+					// only moves like (2,1) or (-1,2)
+					if (Math.abs(rowMove) != Math.abs(colMove)) {
+						// valid key on the dialpad?
+						if (
+							rowIdx + rowMove >= 0 &&
+							rowIdx + rowMove <= 3 &&
+							colIdx + colMove >= 0 &&
+							colIdx + colMove <= 2 &&
+							dialpad[rowIdx + rowMove][colIdx + colMove] != undefined
+						) {
+							nearbyKeys.push(
+								dialpad[rowIdx + rowMove][colIdx + colMove]
+							);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return nearbyKeys;
 }
 
 function countPaths(startingDigit,hopCount) {
-	// TODO: given the digit/key to start from and
-	// the number of hops to take, return a count
-	// of all the possible paths that could be
-	// traversed
-	return 0;
+	if (hopCount == 0) return 1;
+	var pathCount = 0;
+	for (let digit of reachableKeys(startingDigit)) {
+		// recursively count all the paths from the
+		// next digit, but with one fewer hops in length
+		pathCount += countPaths(digit,hopCount-1);
+	}
+	return pathCount;
 }
 
 function listAcyclicPaths(startingDigit) {
-	// TODO: given the digit/key to start from,
-	// return a list of the distinct acyclic
-	// paths that are possible to traverse
-	//
-	// e.g. [
-	//   [4, 3, 8, 1, 6, 7, 2, 9],
-	//   [4, 3, 8, 1, 6, 0],
-	//   ...
-	// ]
-	return [];
+	var paths = [];
+	var nextHops = reachableKeys(startingDigit);
+	for (let nextHop of nextHops) {
+		// init a new path
+		let path = [startingDigit,nextHop];
+		// follow the path as far as possible
+		followPath(path,paths);
+	}
+	return paths;
+}
+
+function followPath(path,paths) {
+	var nextHops = reachableKeys(path[path.length-1]);
+	var pathForwardFound = false;
+	for (let nextHop of nextHops) {
+		// is this next key NOT already in the collected
+		// path?
+		if (!path.includes(nextHop)) {
+			pathForwardFound = true;
+			let nextPath = [...path,nextHop];
+			// recursively keep following the path
+			// as far as possible
+			followPath(nextPath,paths);
+		}
+	}
+	// no forward paths found?
+	if (!pathForwardFound) {
+		// this path is complete, so save it
+		paths.push(path);
+	}
 }
