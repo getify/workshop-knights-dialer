270a81c7784a680b0bc1ff47986bb6910d92fbcd
caprizone6 <caprizone6@gmail.com>
2023-02-25 17:14:29 -0600

    precompute reachableKeys()

--- js/dialer.js
+++ js/dialer.js
@@ -7,44 +7,22 @@ export default {
 
 // ****************************
 
-var dialpad = [
-	[1, 2, 3],
-	[4, 5, 6],
-	[7, 8, 9],
-	[ , 0,  ]
-]
+var nearbyKeys = [
+	[4,6],
+	[6,8],
+	[7,9],
+	[4,8],
+	[3,9,0],
+	[],
+	[1,7,0],
+	[2,6],
+	[1,3],
+	[2,4]
+];
 
 function reachableKeys(startingDigit) {
-	// return which digits a Knight's move
-	// can hop to from a given starting digit/key
-	//
-	// e.g. 3 -> [ 4, 8 ]
-	//      4 -> [ 3, 9, 0 ]
-	//      5 -> []
-
-	var nearbyKeys = []
-
-	for (let [ rowIdx, row] of dialpad.entries()) {
-		let  colIdx = row.indexOf(startingDigit)
-		if (colIdx != -1) {
-			for (let rowMove of [-2, -1, 1, 2]) {
-				for (let colMove of  [-2, -1, 1, 2]) {
-					if (Math.abs(rowMove) != Math.abs(colMove)) {
-						if (
-							rowIdx + rowMove >= 0 &&
-							rowIdx + rowMove <= 3 &&
-							colIdx + colMove >= 0 &&
-							colIdx + colMove <= 3 &&
-							dialpad[rowIdx + rowMove][colIdx + colMove] != undefined
-						){
-							nearbyKeys.push(dialpad[rowIdx + rowMove][colIdx + colMove])
-						}
-					}
-				}
-			}
-		}
-	}
-	return nearbyKeys
+	// precompute data if you can before starting writing algorithm
+	return nearbyKeys[startingDigit];
 }
 
 function countPaths(startingDigit,hopCount) {
